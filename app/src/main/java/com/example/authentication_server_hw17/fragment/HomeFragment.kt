aa82c94338f985c28afc7f75fc0a43a0b50a3465
Lasha-Ilashvili <ilashvililasha@gmail.com>
2023-12-13 09:07:00 +0400

    Navigation update

--- app/src/main/java/com/example/authentication_server_hw17/fragment/HomeFragment.kt
+++ app/src/main/java/com/example/authentication_server_hw17/fragment/HomeFragment.kt
@@ -1,9 +1,19 @@
 package com.example.authentication_server_hw17.fragment
 
+import androidx.navigation.fragment.findNavController
 import com.example.authentication_server_hw17.base.BaseFragment
 import com.example.authentication_server_hw17.databinding.FragmentHomeBinding
 
 
 class HomeFragment : BaseFragment<FragmentHomeBinding>(FragmentHomeBinding::inflate) {
+
+
+    override fun setListeners() {
+        binding.btnLogout.setOnClickListener {
+            findNavController().navigate(
+                HomeFragmentDirections.actionHomeFragmentToLoginFragment()
+            )
+        }
+    }
 
 }
--- app/src/main/java/com/example/authentication_server_hw17/fragment/RegisterFragment.kt
+++ app/src/main/java/com/example/authentication_server_hw17/fragment/RegisterFragment.kt
@@ -1,10 +1,16 @@
 package com.example.authentication_server_hw17.fragment
 
+import androidx.navigation.fragment.findNavController
 import com.example.authentication_server_hw17.base.BaseFragment
 import com.example.authentication_server_hw17.databinding.FragmentRegisterBinding
 
 
 class RegisterFragment : BaseFragment<FragmentRegisterBinding>(FragmentRegisterBinding::inflate) {
 
+    override fun setListeners() {
 
+        binding.btnRegister.setOnClickListener {
+            findNavController().popBackStack()
+        }
+    }
 }
